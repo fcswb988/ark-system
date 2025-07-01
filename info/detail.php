65e5e32006727a0007fcccf98cc6a84dae0ce1bf
fcswb988 <fcswb988@yahoo.co.jp>
2025-07-01 19:17:46 +0900

    Add files via upload

---
+++ info/detail.php
@@ -0,0 +1,33 @@
+<?php
+
+require("../system2/basicFunc.php");
+require("../system2/flex/flexFunc.php");
+require("../lib/commonClass.php");
+require("../lib/infoClass.php");
+
+
+$GLOBALS["basicFunc"] = new basicFunc();
+
+$mysqlConnect = new mysqlConnect();
+$mysqlConnect -> open_mysql();
+
+$form = allRequest();
+$replace = array();
+
+$commonClass = new commonClass();
+$commonClass -> login_check();
+$replace["page_slug"] = "info";
+$replace = $commonClass -> load_basic($replace);
+
+$infoClass = new infoClass();
+$infoClass -> w_customers = $commonClass -> w_customers;
+
+$replace = $infoClass -> load_detail($replace);
+
+$replace["page_title"] = "お知らせ";
+$replace["page_title_sub"] = "お知らせ";
+
+$file_name = "./detail.tpl.html";
+
+print $GLOBALS["basicFunc"] -> make_html_output($replace, $file_name);
+exit;
